f62455b3d4e64fa8934fb7c24be77e6c73a629e1
00nahyeonKim <nahyeon000918@gmail.com>
2026-02-12 16:26:55 +0900

    두번째 커밋

--- coin.py
+++
@@ -1,8 +0,0 @@
-import requests as req
-
-url = "https://api4.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-response_data = req.get(url).json()
-
-price = float(response_data['price'])
-
-print(str(price * 1450) + '원')
