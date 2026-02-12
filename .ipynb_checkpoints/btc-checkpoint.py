3c1c30e05806dff9a5487db98bcf5d0b02cce5d8
00nahyeonKim <nahyeon000918@gmail.com>
2026-02-12 14:55:32 +0900

    또품

--- .ipynb_checkpoints/btc-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import requests as req
-
-url = "https://api4.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-response_data = req.get(url).json()
-
-price = float(response_data['price'])
-
-won = str(int(price * 1450))
-print(won[:1] + '억' + won[1:5] + '만' + won[5:] + '원')
